ba2fc3e79d936293052974344a92763a0f02d262
V <vik.bobkov@gmail.com>
2013-11-07 20:11:02 -0800

    remote upload to magmi

--- magmi/post_to_csv.php
+++ magmi/post_to_csv.php
@@ -1,5 +1,5 @@
 <?php
 print_r($_POST['file_content']);
-file_put_content('state/magmi_import.csv', $_POST['file_content']);
+file_put_contents('state/magmi_import.csv', $_POST['file_content']);
 return;
 ?>
