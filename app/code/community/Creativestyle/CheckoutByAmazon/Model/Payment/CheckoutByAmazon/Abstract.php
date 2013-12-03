6dc9fff956e36152788b88c8c94bf63acd6c9dc5
V <vik.bobkov@gmail.com>
2013-12-03 15:47:42 -0800

    amazon payments now available, after a small hack in the Creativestyle module

--- app/code/community/Creativestyle/CheckoutByAmazon/Model/Payment/CheckoutByAmazon/Abstract.php
+++ app/code/community/Creativestyle/CheckoutByAmazon/Model/Payment/CheckoutByAmazon/Abstract.php
@@ -68,6 +68,57 @@ class Creativestyle_CheckoutByAmazon_Model_Payment_CheckoutByAmazon_Abstract ext
         return $checkResult->isAvailable;
     }
 
+    public function isApplicableToQuote($quote, $checksBitMask)
+    {
+        if ($checksBitMask & self::CHECK_USE_FOR_COUNTRY) {
+            if (!$this->canUseForCountry($quote->getBillingAddress()->getCountry())) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_USE_FOR_CURRENCY) {
+            if (!$this->canUseForCurrency($quote->getStore()->getBaseCurrencyCode())) {
+                return false;
+            }
+        }
+        // if ($checksBitMask & self::CHECK_USE_CHECKOUT) {
+            // if (!$this->canUseCheckout()) {
+                // return false;
+            // }
+        // }
+        if ($checksBitMask & self::CHECK_USE_FOR_MULTISHIPPING) {
+            if (!$this->canUseForMultishipping()) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_USE_INTERNAL) {
+            if (!$this->canUseInternal()) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_ORDER_TOTAL_MIN_MAX) {
+            $total = $quote->getBaseGrandTotal();
+            $minTotal = $this->getConfigData('min_order_total');
+            $maxTotal = $this->getConfigData('max_order_total');
+            if (!empty($minTotal) && $total < $minTotal || !empty($maxTotal) && $total > $maxTotal) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_RECURRING_PROFILES) {
+            if (!$this->canManageRecurringProfiles() && $quote->hasRecurringItems()) {
+                return false;
+            }
+        }
+        if ($checksBitMask & self::CHECK_ZERO_TOTAL) {
+            $total = $quote->getBaseSubtotal() + $quote->getShippingAddress()->getBaseShippingAmount();
+            if ($total < 0.0001 && $this->getCode() != 'free'
+                && !($this->canManageRecurringProfiles() && $quote->hasRecurringItems())
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     /**
      * Instantiate state and set it to state object
      * @param string $paymentAction
