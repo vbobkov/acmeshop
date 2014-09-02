ee72b9acec3f08a6ea34db1c5f540db4ca1bce7b
V <vik.bobkov@gmail.com>
2014-09-02 10:39:15 -0700

    overwrote magento core - disabled fetching of product collection search results (using my own search method)

--- app/code/local/ACMElectronics/CatalogSearch/Block/Result.php
+++ app/code/local/ACMElectronics/CatalogSearch/Block/Result.php
@@ -1,13 +1,17 @@
 <?php
 class ACMElectronics_CatalogSearch_Block_Result extends Mage_CatalogSearch_Block_Result {
-    protected function _getProductCollection()
-    {
-        if (is_null($this->_productCollection)) {
+	protected function _getProductCollection() {
+		return null;
+		if (is_null($this->_productCollection)) {
 			$this->_productCollection = $this->getListBlock()->getLoadedProductCollection();
 			// $this->_productCollection = Mage::getModel('catalogsearch/fulltext')->getCollection();
 			// $this->_productCollection = new Mage_CatalogSearch_Model_Resource_Query_Collection();
-        }
+		}
 
-        return $this->_productCollection;
-    }
+		return $this->_productCollection;
+	}
+
+	public function getResultCount() {
+		return 1;
+	}
 }
