7c71c404edc31583b319c09940722d8a8d4228eb
V <vik.bobkov@gmail.com>
2014-09-10 16:01:36 -0700

    reset url keys script

--- reset_url_keys.php
+++ reset_url_keys.php
@@ -14,10 +14,12 @@ function generateURLKey($product_title) {
 require('app/Mage.php');
 Mage::app();
 $amount = 0;
+$total = 0;
 $model = Mage::getModel('catalog/product');
 echo "fetching the entire product collection\n";
 $products = $model->getCollection()->addAttributeToSelect('url_key')->addAttributeToSelect('name');
 foreach ($products as $product) {
+	$total++;
 	if($product->getUrlKey() != generateURLKey($product->getName())) {
 		$prev_key = $product->getUrlKey();
 		// $model->load($product->getId());
@@ -25,6 +27,6 @@ foreach ($products as $product) {
 		$product->setUrlKey($product->getName())->save();
 		// set_time_limit();
 		$amount++;
-		echo "[" . $product->getName() . "] " . $prev_key . " set to " . $product->getUrlKey() . " [total changed: " . $amount . "]\n";
+		echo "[" . $product->getName() . "] " . $prev_key . " set to " . $product->getUrlKey() . " [" . $amount . "/" . $total . "]\n";
 	}
 }
