00d360fa3970127e1c5a4e189b93c905151bb05f
V <vik.bobkov@gmail.com>
2014-09-10 14:36:05 -0700

    reset url keys script

--- reset_url_keys.php
+++ reset_url_keys.php
@@ -3,11 +3,12 @@ require('app/Mage.php');
 Mage::app();
 $amount = 0;
 $model = Mage::getModel('catalog/product');
+echo "fetching the entire product collection\n";
 $products = $model->getCollection();
 foreach ($products as $product) {
     $model->load($product->getId());
     $product->setUrlKey($model->getName())->save();
     // set_time_limit();
     $amount++;
-	echo $amount . ' product URLs processed';
+	echo $amount . " product URLs processed\n";
 }
