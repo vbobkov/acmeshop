fdcad4b4e0696bbc8102c1d78a2c55bead40deb0
V <vik.bobkov@gmail.com>
2014-09-10 15:30:57 -0700

    reset url keys script

--- reset_url_keys.php
+++ reset_url_keys.php
@@ -1,14 +1,30 @@
 <?php
+function generateURLKey($product_title) {
+	return strtolower(
+		preg_replace('/-{2,}/', '-',
+			preg_replace('/^-/', '',
+				preg_replace('/-$/', '',
+					preg_replace('/[^0-9A-Za-z-]/', '-', $product_title)
+				)
+			)
+		)
+	);
+}
+
 require('app/Mage.php');
 Mage::app();
 $amount = 0;
 $model = Mage::getModel('catalog/product');
 echo "fetching the entire product collection\n";
-$products = $model->getCollection();
+$products = $model->getCollection()->addAttributeToSelect('url_key')->addAttributeToSelect('name');
 foreach ($products as $product) {
-    $model->load($product->getId());
-    $product->setUrlKey($model->getName())->save();
-    // set_time_limit();
-    $amount++;
-	echo $amount . " product URLs processed\n";
+	if($product->getUrlKey() != generateURLKey($product->getName())) {
+		$prev_key = $product->getUrlKey();
+		// $model->load($product->getId());
+		// $product->setUrlKey($model->getName())->save();
+		$product->setUrlKey($product->getName())->save();
+		// set_time_limit();
+		$amount++;
+		echo "[" . $product->getName() . "] " . $prev_key . " set to " . $product->getUrlKey() . " [total changed: " . $amount . "]\n";
+	}
 }
